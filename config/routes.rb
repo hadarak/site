8cef31373b0a5dc09b84c50fdf82e8effe2439ef
Lucas <lucas.roriz@hotmail.com>
2015-11-24 00:24:34 -0200

    v1.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments
   resources :ideas
-#  root :to => redirect('/ideas')
+  root :to => redirect('/ideas')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,5 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root :to => "home#index"
+  # root :to => "home#index"
 end
